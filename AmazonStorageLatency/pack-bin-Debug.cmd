68c17b814a81f83adfec957fa3f188b4d32201d9
devizer <devizer@sandbox.com>
2016-09-29 22:22:05 +0300

    update

--- AmazonStorageLatency/pack-bin-Debug.cmd
+++ AmazonStorageLatency/pack-bin-Debug.cmd
@@ -1,2 +1,5 @@
-del S3Samples-bin-Debug.rar
-winrar a S3Samples-bin-Debug.rar bin\Debug -m5 -s -ep -apS3Samples -x*.xml
+mkdir ..\Releases 1>nul 2>nul
+del ..\Releases\AmazonStorageLatency-bin-Debug.rar
+pushd bin\Debug
+winrar a ..\..\..\Releases\AmazonStorageLatency-bin-Debug.rar . -r -m5 -s -ep1 -apAmazonStorageLatency -x*.xml
+popd
