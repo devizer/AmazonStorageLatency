e6ea5b636dc32486382300cb4c92ca62e052ea99
devizer <devizer@sandbox.com>
2016-09-30 15:36:55 +0300

    update

--- pack-bin-Debug.cmd
+++ pack-bin-Debug.cmd
@@ -1,5 +1,5 @@
-mkdir ..\Releases 1>nul 2>nul
-del ..\Releases\AmazonStorageLatency-bin-Debug.rar
-pushd bin\Debug
+mkdir .\Releases 1>nul 2>nul
+del .\Releases\AmazonStorageLatency-bin-Debug.rar
+pushd AmazonStorageLatency\bin\Debug
 winrar a ..\..\..\Releases\AmazonStorageLatency-bin-Debug.rar . -r -m5 -s -ep1 -apAmazonStorageLatency -x*.xml
 popd
